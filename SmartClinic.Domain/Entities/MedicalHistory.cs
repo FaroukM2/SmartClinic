07148d48ad68e85ad29b4872bae18aa4e851bcbe
fmrok2 <faroukmohamed1011@gmail.com>
2026-08-05 06:56:22 +0300

    initial project setup

---
+++ SmartClinic.Domain/Entities/MedicalHistory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartClinic.Domain.Entities
+{
+    internal class MedicalHistory
+    {
+    }
+}
